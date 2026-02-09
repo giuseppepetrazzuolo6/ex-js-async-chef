c6a6d5f4c9dfc3e30e29f1c6fe0fc45adb51f9cc
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-09 11:54:20 +0100

    add getChefBirthday using async/await
    Implemented fetchJson helper for API calls.
    Added getChefBirthday function to fetch a recipe by id,
    retrieve the related chef via userId and return the chef's birth date

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ Recuperare la ricetta da https://dummyjson.com/recipes/{id}
 Estrarre la proprietà userId dalla ricetta
 Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
 Restituire la data di nascita dello chef*/
+
+async function fetchJson(url) {
+    const response = await fetch(url)
+    const obj = await response.json()
+    return obj
+}
+
+async function getChefBirthday(id) {
+    const ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`)
+    const userId = ricetta.userId
+    const chef = await fetchJson(`https://dummyjson.com/users/${userId}`)
+    const chefBirthday = chef.birthDate
+    return chefBirthday
+
+}
+
+(async () => {
+    const chefBirthday = await getChefBirthday(1)
+    console.log('Data di nascita dello chef:', chefBirthday)
+})()
