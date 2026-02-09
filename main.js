f9f3cf9475078a672b1161dad8b7d4357b3df783
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-02-09 11:18:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+/*In questo esercizio, utilizzerai async/await per creare la funzione getChefBirthday(id). 
+Questa funzione accetta un id di una ricetta e deve:
+Recuperare la ricetta da https://dummyjson.com/recipes/{id}
+Estrarre la proprietà userId dalla ricetta
+Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
+Restituire la data di nascita dello chef*/
